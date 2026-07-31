26842e70f01b258edb342880c80b0e34a52a7135
CVK0406 <khoiche0@gmail.com>
2026-07-31 20:30:32 +0700

    feat: initial CachyOS + Hyprland + Caelestia dotfiles snapshot

---
+++ .config/fish/config.fish
@@ -0,0 +1,50 @@
+if status is-interactive
+    # Starship custom prompt
+    command -v starship &> /dev/null && starship init fish | source
+
+    # Direnv + Zoxide
+    command -v direnv &> /dev/null && direnv hook fish | source
+    command -v zoxide &> /dev/null && zoxide init fish --cmd cd | source
+
+    # Better ls
+    command -v eza &> /dev/null && alias ls='eza --icons --group-directories-first -1'
+
+    # Abbrs
+    abbr lg 'lazygit'
+    abbr gd 'git diff'
+    abbr ga 'git add .'
+    abbr gc 'git commit -am'
+    abbr gl 'git log'
+    abbr gs 'git status'
+    abbr gst 'git stash'
+    abbr gsp 'git stash pop'
+    abbr gp 'git push'
+    abbr gpl 'git pull'
+    abbr gsw 'git switch'
+    abbr gsm 'git switch main'
+    abbr gb 'git branch'
+    abbr gbd 'git branch -d'
+    abbr gco 'git checkout'
+    abbr gsh 'git show'
+
+    abbr l 'ls'
+    abbr ll 'ls -l'
+    abbr la 'ls -a'
+    abbr lla 'ls -la'
+
+    # Custom colours
+    cat ~/.local/state/caelestia/sequences.txt 2> /dev/null
+
+    # For jumping between prompts in foot terminal
+    function mark_prompt_start --on-event fish_prompt
+        echo -en "\e]133;A\e\\"
+    end
+
+    # Custom fish config
+    set -q XDG_CONFIG_HOME && set -l cConf $XDG_CONFIG_HOME/caelestia || set -l cConf $HOME/.config/caelestia
+    source $cConf/user-config.fish 2> /dev/null
+end
+alias matrix 'rusty-rain -s -g jap -C 0,60,150 -H white -S 10,180'
+alias dotfiles 'git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+alias dots-sync 'pacman -Qqen > ~/dotfiles-export/pkglist.txt && pacman -Qqem > ~/dotfiles-export/aur-pkglist.txt && dotfiles add -u && dotfiles commit -m "sync $(date +%F)" && dotfiles push'
+
